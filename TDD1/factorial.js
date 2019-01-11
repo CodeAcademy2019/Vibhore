75831a6b5cda853292c5346cdaa0457f24d33a8a
vibhore.vg <vibhore97@gmail.com>
2019-01-11 18:21:48 +0530

    two test case

--- TDD1/factorial.js
+++ TDD1/factorial.js
@@ -1,11 +1,13 @@
 var factorial = (n) => {
-
+    if(n<0){
+        return "input rejected";
+    }else {
     if (n<=1)
         return 1;
     else {
         return n * factorial(n-1);
     }
-    
+}
     };
     
     module.exports = factorial;
--- TDD1/factorial.test.js
+++ TDD1/factorial.test.js
@@ -4,3 +4,7 @@ const factorial = require('./factorial.js');
 test ('factorial should return some value',() => {
     expect(factorial(4)).toBe(24);
 });
+
+test('factorial should return not acceptable value',() => {
+    expect(factorial(-1)).toBe('input rejected');
+});
