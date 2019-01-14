b8022f7ed0108c494f1a8a5b7993c44753c16c72
vibhore.vg <vibhore97@gmail.com>
2019-01-14 10:40:59 +0530

    tagged_temp.js:code fixed

--- HELLO_ES6/tagged_temp.js
+++ HELLO_ES6/tagged_temp.js
@@ -1,17 +1,32 @@
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
 
-function html  (x, ...parts) {
+function html  (y, ...parts) {
     
-    var rp ={"'": '&apos;', '"': '&quot;', '<': "&lt;", ">": "&gt;", "&": "&amp"};
-    var s1 = x.map( (x,rp) => { return rp[x]||x});
-    return x[0]+s1[0]+x[1]+s1[1]+x[2];
+    //var rp = {"'": '&apos;', '"': '&quot;', '<': "&lt;", ">": "&gt;", "&": "&amp"};
     
+    //return p1;
+    var s1 = parts.map( (x) => { 
+       return x.split(" ").map((y) => { 
+        return y.replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/'/g, "&apos;")
+       .replace(/"/g, "&quot;");});
+    });
+    //return s1;
+    var j1 = s1[0].join(" ");
+    var j2 = s1[1].join(" ");
+    //return j2;
     
-
+    return y[0]+j1+y[1]+j2+y[2];
+    
 
 };
+// console.log(html`<b>${} says</b>: "${process.argv[3]}"`);
 
+
+//"domenic" and the comment is "<dl> is a fun tag",
 
 
 /*
