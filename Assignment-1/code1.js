05829bb886b0b89890967bbd6df1714ccdbec25e
vibhore.vg <vibhore97@gmail.com>
2019-01-22 10:02:05 +0530

    Commit with failed test cases

---
+++ Assignment-1/code1.js
@@ -0,0 +1,32 @@
+const roll = (arr) => {
+
+    const cumm_score = 0;
+    return 90;
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+// Sample Test
+
+// Testcase1:
+// Rolls: 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6
+// Score: 90
+
+// Testcase2:
+// Rolls: 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10
+// Score: 30
+
+// Testcase3:
+// Rolls: 6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+// Score: 16
+module.exports = roll;
---
+++ Assignment-1/code1.test.js
@@ -0,0 +1,18 @@
+const roll = require('./code1');
+
+const test1 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+const test2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
+const test3 = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+describe('Bowling Score Tests',()=>{
+    it('This test checks the score of player ',()=>{
+        return expect(roll(test1)).toEqual(90);
+    });
+    it('This test checks the score of player ',()=>{
+        return expect(roll(test2)).toEqual(30);
+    });
+    it('This test checks the score of player ',()=>{
+        return expect(roll(test3)).toEqual(60);
+    });
+    
+});
